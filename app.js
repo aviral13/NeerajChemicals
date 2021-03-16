ee3aa11d1e2896c2cb7340bae6a936242f9ec2e6
Aviral Garg <aviral.13g@gmail.com>
2021-03-16 15:12:11 +0530

    Added gitignore, Procfile and updates ports

--- app.js
+++ app.js
@@ -45,11 +45,15 @@ const aos=require("aos")
 //   })
 // })
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
 
 
- app.listen(3000,function(){
+ app.listen(port,function(){
    console.log("Server is running on port 3000");
  })
 
